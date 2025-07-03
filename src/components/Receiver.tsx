6f598b5108d863981ba1a2dd186875d3bd89c7cf
Alphparadox <anilpj120@gmail.com>
2025-07-03 20:33:15 +0530

    setup

---
+++ src/components/Receiver.tsx
@@ -0,0 +1,8 @@
+export function Receiver() {
+    return (
+        <div>
+        <h1>Receiver Component</h1>
+        <p>This is the receiver component.</p>
+        </div>
+    );
+    }
---
+++ src/components/Sender.tsx
@@ -0,0 +1,8 @@
+export function Sender() { 
+    return (
+        <div>
+            <h1>Sender Component</h1>
+            <p>This is the sender component.</p>
+        </div>
+    );
+}
